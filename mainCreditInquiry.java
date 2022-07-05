ec4b617c4142c24a2d3b2022641b5b134b02f2b6
Giovanna Alves de Farias <giovannaalves02@hotmail.com>
2022-07-04 21:54:12 -0300

    Criação das classes AccountRecord e mainCreditInquiry

---
+++ mainCreditInquiry.java
@@ -0,0 +1,27 @@
+import java.io.IOException;
+ 
+public class mainCreditInquiry {
+    
+    // public class ClassNotFoundExceptionDemo {
+        
+        // private static final String DRIVER_CLASS = “credityInquiry.java”
+        
+        public static void main(String[] args) throws IOException  {
+            
+            // TODO AUTO-generated method sub
+            creditlnquiry app = new creditlnquiry();
+            app.processRequest();
+            
+            /* 
+            try {
+                Class.forName(“credityInquiry”);
+            } catch () {
+                throw new Exception(e);
+            }
+            */
+
+    }
+   
+}
+ 
+//}
